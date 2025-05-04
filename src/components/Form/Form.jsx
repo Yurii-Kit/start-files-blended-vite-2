13e463fa1403ba22b7fcde5bd54a347be8ee2a33
Alona Harnyk <alyonagarnyk@gmail.com>
2025-05-04 10:48:05 +0300

    add form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,63 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+// import { useState } from 'react';
 
-const Form = () => {
-  return <h2>Form</h2>;
+// const Form = ({ onSubmit }) => {
+//   const [value, setValue] = useState('');
+
+//   const submitHandler = e => {
+//     e.preventDefault();
+//     if(!value.trim()) return;
+//     onSubmit(value)
+//     setValue('')
+//   };
+
+//   return (
+//     <form className={style.form} onSubmit={submitHandler}>
+//       <button className={style.button} type="submit">
+//         <FiSearch size="16px" />
+//       </button>
+
+//       <input
+//         className={style.input}
+//         placeholder="What do you want to write?"
+//         name="search"
+//         required
+//         autoFocus
+//         value={value}
+//         onChange={e => setValue(e.target.value)}
+//       />
+//     </form>
+//   );
+// };
+
+// export default Form;
+
+
+const Form = ({ onSubmit }) => {
+  const submitHandler = e => {
+    e.preventDefault();
+    const inputValue = e.target.elements['search'].value
+    if(!inputValue.trim()) return;
+    onSubmit(inputValue)
+    e.target.reset()
+  };
+
+  return (
+    <form className={style.form} onSubmit={submitHandler}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+      />
+    </form>
+  );
 };
 
 export default Form;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,16 @@
-import Text from '../components/Text/Text';
+// import Text from '../components/Text/Text';
+import Form from '../components/Form/Form'
 
 const Todos = () => {
+
+  const addNewTodo = inputValue => {
+    console.log(inputValue); // має вивести значення інпуту під час сабміту форми
+  };
+
   return (
     <>
-      <Text textAlign="center">There are no any todos ...</Text>
+     <Form onSubmit={addNewTodo} />
+      {/* <Text textAlign="center">There are no any todos ...</Text> */}
     </>
   );
 };
